37bc3e34bddbc3f615fc315967e3d3c30aee20d3
Jie Yan <jieyan620@gmail.com>
2020-08-05 18:00:13 -0700

    add timer

--- script.js
+++ script.js
@@ -3,15 +3,29 @@ var op2 = document.getElementById('op2');
 var op3 = document.getElementById('op3');
 var op4 = document.getElementById('op4');
 
-var app{
-   questions:[
-     { q:'question one', options: [' 1. answer one1', '2. answer two1', '3. answer three1','4. answer four1'],answer:1}
-     { q: 'question two', options: [' 1. answer one2', '2. answer two2', '3. answer three2', '4. answer four2'], answer: 1 } 
-     { q: 'question three', options: [' 1. answer one3', '2. answer two3', '3. answer three3', '4. answer four3'], answer: 1 }
-     { q: 'question four', options: [' 1. answer one4', '2. answer two4', '3. answer three4', '4. answer four4'], answer: 1 }
-    ]
-   load:function name(params) {
+// var app{
+//    questions:[
+//      { q:'question one', options: [' 1. answer one1', '2. answer two1', '3. answer three1','4. answer four1'],answer:1}
+//      { q: 'question two', options: [' 1. answer one2', '2. answer two2', '3. answer three2', '4. answer four2'], answer: 1 } 
+//      { q: 'question three', options: [' 1. answer one3', '2. answer two3', '3. answer three3', '4. answer four3'], answer: 1 }
+//      { q: 'question four', options: [' 1. answer one4', '2. answer two4', '3. answer three4', '4. answer four4'], answer: 1 }
+//     ]
+//    load:function name(params) {
      
-   }  
+//    }  
 
-}
+// }
+
+
+
+// timer on Nav
+var timeleft = 60;
+var downloadTimer = setInterval(function () {
+   if (timeleft <= 0) {
+      clearInterval(downloadTimer);
+      document.getElementById("timer").innerHTML = "Finished";
+   } else {
+      document.getElementById("timer").innerHTML = timeleft;
+   }
+   timeleft -= 1;
+}, 1000);
