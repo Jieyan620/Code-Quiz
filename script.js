71c3f94f472a2e9a67544e8a277cae7fead01455
Jie Yan <jieyan620@gmail.com>
2020-08-06 17:33:10 -0700

    set answers ready

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var quiz_link = `https://opentdb.com/api.php?amount=10&category=18&difficulty=ea
 
 var quizNumber = 0
 
-
+console.log()
 // timer on Nav
 var timeleft = 60;
 var downloadTimer = setInterval(function () {
@@ -22,26 +22,67 @@ var downloadTimer = setInterval(function () {
 
 // quiz api https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple
 function getQuizs(quizNumber) {
+
    fetch(quiz_link)
       .then(r => r.json())
       .then((quizs) => {
          const quizsArray = quizs.results
          console.log(quizsArray)
-
-
+         showQuiz(quizsArray)
       })
 }
 
+// randomize the answers
+function shuffleArray(quizAnswers) {
+   for (var i = quizAnswers.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = quizAnswers[i];
+      quizAnswers[i] = quizAnswers[j];
+      quizAnswers[j] = temp;
+   }
+   return quizAnswers
+}
+
+function showQuiz(quizsArray) {
+   // get all answers in one array
+   var quizAnswers = []
+   var worngAnswers = quizsArray[quizNumber].incorrect_answers
+   quizAnswers = worngAnswers.map(
+      function add(incorrect_answer) {
+         return {
+            answerBool: false,
+            text: incorrect_answer
+         }
+   })
+   var answer = { answerBool: true, text: quizsArray[quizNumber].correct_answer }
+   quizAnswers.push(answer)
+   
+   shuffleArray(quizAnswers)
+
+   // show in html
+   
+   console.log(quizAnswers)
+}
 
 
 // add event listener to whole page
 
-document.addEventListener('click'. ({target}) => {
+function eventListener() {
+   document.addEventListener('click', ({ target }) => {
+
+      // if timer
+
+      if (target.id === "op1" || "op2" || "op3" || "op4") {
+
+      }
 
 
-   
-})
+
+   })
+}
 
 
 
 
+
+getQuizs()
